74fe57fb59ee7b1992006407586d5f29afc98fa2
lgs85 <lewisspurgin@gmail.com>
2017-12-09 14:08:46 +0000

    Edits

--- Scripts/GWAS_sims_CLEAN.R
+++ Scripts/GWAS_sims_CLEAN.R
@@ -9,5 +9,5 @@ h1 <- clean_heritability_data(h1)
 q1 <- clean_heritability_data(q1)
 
 h4 <- clean_heritability_data(h4,Ngens = T)
-g4 <- clean_heritability_data(g4,Ngens= T)
+q4 <- clean_heritability_data(q4,Ngens= T)
 
