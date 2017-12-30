5e722b52aee41a76b88e6e020e8205ff430f250b
lgs85 <lewisspurgin@gmail.com>
2017-12-30 16:38:41 +0000

    Edits

--- Scripts/GWAS_sims_CLEAN.R
+++ Scripts/GWAS_sims_CLEAN.R
@@ -14,3 +14,7 @@ q2 <- clean_heritability_data(q2,Ngens = T)
 h4 <- clean_heritability_data(h4,Ngens = T)
 q4 <- clean_heritability_data(q4,Ngens= T)
 
+g4$Nids <- g4$Nids * 10
+h4$Nids <- h4$Nids * 10
+
+g4$Nsnps[g4$Nsnps > 150000] <- 2e05
